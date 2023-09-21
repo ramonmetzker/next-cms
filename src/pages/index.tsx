4c309a366ab7be71b4d274c88eda95f125c0c0e8
Ramon Metzker <ramonmetzker12@gmail.com>
2023-09-20 21:12:14 -0400

    fix: update trpc posts route

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 export default function Home() {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  const hello = api.posts.hello.useQuery({ text: "from tRPC" });
 
   const user = useUser();
 
